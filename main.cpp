e74d8991b9ba43ec99f6b118ee9824829e2db25b
kaashmonee <skandakk@gmail.com>
2017-05-27 21:41:14 -0500

    opencv works

--- main.cpp
+++ main.cpp
@@ -13,19 +13,30 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgcodecs.hpp>
 
 using namespace std;
+using namespace cv;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
-    int number;
-    cout<<"What is the number? ";
-    cin>>number;
-    if (number%2==0) cout<<"The number is even!";
-    else cout<<"The number is odd :(";
+    Mat image;
+    image = imread("/home/skanda/Documents/ImageMemes/images/image1.png", CV_LOAD_IMAGE_COLOR);
     
+    if (!image.data) {
+        cout<<"Couldn't find image.";
+        return -1;
+    }
+    
+    namedWindow("window", CV_WINDOW_AUTOSIZE); //creates a window to show the image
+    imshow("window", image); //shows the image inside of the window
+    imwrite("/home/skanda/Documents/ImageMemes/images/result.png", image); //stores the image as image.png
+    waitKey(0); //i think this is the enter key
     return 0;
 }
 
